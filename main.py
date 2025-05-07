7ef155c04f7388de4516a544a71d1135ca371afb
raphaelKnz <rafaelkenzoito@gmail.com>
2025-05-07 16:49:44 -0300

    utilizacao da fastapi com query

--- main.py
+++ main.py
@@ -6,3 +6,25 @@ app = FastAPI()
 @app.get('/api/hello')
 def bye_planet():
     return {'hello': 'world'}
+
+@app.get('/api/restaurante')
+def procurar_restaurantes(restaurante: str = Query(None)):
+    url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        dados_json = response.json()
+        lista_restaurantes = []
+        if restaurante is None:
+            return {'Dados': dados_json}
+
+        for item in dados_json:
+            if item['Company'] == restaurante:
+                lista_restaurantes.append({
+                "Item" : item['Item'],
+                "Preco": item['price'],
+                "Descricao" : item['description']
+            })
+        return{'Restaurante':restaurante, 'Cardápio':lista_restaurantes }
+    else:
+        return {f'o código do erro: {response.status_code}'}
