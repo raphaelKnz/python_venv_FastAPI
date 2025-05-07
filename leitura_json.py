9d0b454dadf5e0598be5d4c3a22de9aaced76fa9
raphaelKnz <rafaelkenzoito@gmail.com>
2025-05-07 12:18:23 -0300

    First Commit

---
+++ leitura_json.py
@@ -0,0 +1,23 @@
+import requests
+import json
+url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
+response = requests.get(url)
+print(response)
+
+if response.status_code == 200:
+    dados_json = response.json()
+    dicionario_restaurantes = {}
+    for item in dados_json:
+        nome_restaurante = item['Company']
+        if nome_restaurante not     in dicionario_restaurantes:
+            dicionario_restaurantes[nome_restaurante] = []
+
+        dicionario_restaurantes[nome_restaurante].append({
+            "Item" : item['Item'],
+            "Preco": item['price'],
+            "Descricao" : item['description']
+        })
+else:
+    print(f'o código do erro: {response.status_code}')
+
+print(dicionario_restaurantes['KFC'])
